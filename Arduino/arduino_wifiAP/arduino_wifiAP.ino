556e5306da00ad37ab890cc58df014f68a7975f1
Oetoekkae <aleksi.kouhi@edu.turkuamk.fi>
2020-01-30 13:41:47 +0200

    arduino reacting to POST and gets color values from request body

--- Arduino/arduino_wifiAP/arduino_wifiAP.ino
+++ Arduino/arduino_wifiAP/arduino_wifiAP.ino
@@ -2,35 +2,35 @@
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 
-/* Set these to your desired credentials. */
+//SSID and password
 const char *ssid = "[REDACTED]";
 const char *password = "realityisalie";
 
 ESP8266WebServer server(80);
 
-//Server configuration
+//Serve configuration
 void setup() {
-	delay(1000);
-	Serial.begin(115200);
-	Serial.println();
-	Serial.print("Configuring access point...");
-	WiFi.softAP(ssid, password);
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  WiFi.softAP(ssid, password);
   //Show address
-	IPAddress myIP = WiFi.softAPIP();
-	Serial.print("AP IP address: ");
-	Serial.println(myIP);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
 
  //Init routes
-	server.on("/", handleRoot);
+  server.on("/", handleRoot);
   server.on("/colors", receiveColors);
 
  //Start server
-	server.begin();
-	Serial.println("HTTP server started");
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-	server.handleClient();
+  server.handleClient();
 }
 
 
@@ -39,5 +39,18 @@ void handleRoot() {
 }
 
 void receiveColors() {
-  
+  Serial.println("You've got mail ");
+  checkMail(server.arg(0), server.arg(1), server.arg(2));
+
+}
+
+void checkMail(String r, String g, String b) {
+  String red, green, blue;
+  String received;
+  red += r;
+  green += g;
+  blue += b;
+  received += "Got these: " + red + ", " + green + ", " + blue;
+  Serial.println(received);
+  server.send(200, "text/plane", received);
 }
