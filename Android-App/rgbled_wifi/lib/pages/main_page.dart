6093e3b3c2eda3ca84aee3245c66af25d5c64fa5
Oetoekkae <aleksi.kouhi@edu.turkuamk.fi>
2020-03-15 00:41:14 +0200

    color picker appearance changes

--- Android-App/rgbled_wifi/lib/pages/main_page.dart
+++ Android-App/rgbled_wifi/lib/pages/main_page.dart
@@ -1,12 +1,14 @@
 import 'package:hex/hex.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:flutter_circle_color_picker/flutter_circle_color_picker.dart';
+//import 'package:flutter_circle_color_picker/flutter_circle_color_picker.dart';
+import 'package:rgbled_wifi/UI/flutter_circle_color_picker.dart';
 
 
 const String A = "0123456789abcdef";
 final _biggerFont = const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black);
 bool _isSending = false;
+Color _bgColor = Colors.blue;
 class MainPage extends StatefulWidget{
   @override
   State createState() => new MainPageState();
@@ -17,38 +19,41 @@ class MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Center(
-              child: CircleColorPicker(
-                initialColor: Colors.blue,
-                onChanged: (color) => _request(color),
-                size: const Size(300, 300),
-                strokeWidth: 2,
-                thumbSize: 32,
+        body: new Material(
+          color: _bgColor,
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: CircleColorPicker(
+                  initialColor: Colors.blue,
+                  onChanged: (color) => _request(color),
+                  size: const Size(300, 300),
+                  strokeWidth: 4,
+                  thumbSize: 36,
+                ),
+                heightFactor: 1.3,
               ),
-              heightFactor: 1.3,
-            ),
-            Expanded(
-              child: Container(
-                child: new ListView.separated(
-                  padding: const EdgeInsets.all(16.0),
-                  itemCount: 5,
-                  itemBuilder: (context, i) =>
-                      ListTile(
-                        leading: Text("SSID: ", style: _biggerFont,
+              Expanded(
+                child: Container(
+                  child: new ListView.separated(
+                    padding: const EdgeInsets.all(16.0),
+                    itemCount: 5,
+                    itemBuilder: (context, i) =>
+                        ListTile(
+                          leading: Text("SSID: ", style: _biggerFont,
+                          ),
+                          title: Text(i.toString(), style: _biggerFont,
+                          ),
                         ),
-                        title: Text(i.toString(), style: _biggerFont,
-                        ),
-                      ),
-                  separatorBuilder: (context, i) => Divider(),
+                    separatorBuilder: (context, i) => Divider(),
 
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-        ), //   <--- image
+      ),//   <--- image
     );
   }
 }
@@ -75,7 +80,11 @@ void getResponse(Response response) {
 void _request(Color hexVal) {
   if(!_isSending) {
     _makePostReq(hexVal);
+    setBgColor(hexVal);
     return;
   }
   print("Can't, still sending this previous");
+}
+void setBgColor(Color bg) {
+  _bgColor = bg;
 }
