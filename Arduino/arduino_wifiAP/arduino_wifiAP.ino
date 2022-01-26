4ad0ea17836469c3183d3be746adf1b83930b469
Oetoekkae <ale.kouhi@hotmail.com>
2022-01-26 17:50:17 +0200

    included fastled library, basic color changing

--- Arduino/arduino_wifiAP/arduino_wifiAP.ino
+++ Arduino/arduino_wifiAP/arduino_wifiAP.ino
@@ -1,15 +1,20 @@
+#include <FastLED.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 
-//led1
-#define R 2
-#define G 13
-#define B 12
-//led2
-#define R2 5
-#define G2 14
-#define B2 16
+
+//Led strip pin and number of leds
+#define LED_PIN 14
+#define NUM_LEDS 164
+//brightness, strip type, strip color order
+#define BRIGHTNESS 64
+#define LED_TYPE WS2812B
+#define COLOR_ORDER RGB
+//led array
+CRGB leds[NUM_LEDS];
+#define UPDATES_PER_SECOND 100
+
 //SSID and password
 const char *ssid = "LED";
 const char *password = "realityisalie";
@@ -19,11 +24,16 @@ ESP8266WebServer server(80);
 
 //Server configuration
 void setup() {
-  delay(1000);
+  //ledstrip init
+  delay(500);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness( BRIGHTNESS );
+  delay(500);
   Serial.begin(115200);
   Serial.println();
   Serial.print("Configuring access point...");
   WiFi.softAP(ssid, password);
+  
   //Show address
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
@@ -31,15 +41,16 @@ void setup() {
  //Init routes
   server.on("/", handleRoot);
   server.on("/colors", receiveColors);
-  server.on("/LOOPRGB!", loopRGB);
-  server.on("/Rainbow", rainbow);
   server.on("/random", randomColor);
  //Start server
   server.begin();
   Serial.println("HTTP server started");
+
 }
 void loop() {
   server.handleClient();
+  FastLED.show();
+  FastLED.delay(1000 / UPDATES_PER_SECOND);
 }
 void handleRoot() {
   server.send(200, "text/html", "<h1>You are connected</h1>");
@@ -59,30 +70,19 @@ void receiveColors() {
     //Serial.println(server.argName(i));
     if(server.argName(i)=="R") {
       r_new = ((server.arg(i).substring(1,4)).toInt());
-      r_new = ((255 - r_new) * 4) + 4;
     } else if (server.argName(i)=="G") {
       g_new = ((server.arg(i).substring(1,4)).toInt());
-      g_new = ((255 - g_new) * 4) + 4;
     } else if (server.argName(i)=="B") {
       b_new = ((server.arg(i).substring(1,4)).toInt());
-      b_new = ((255 - b_new) * 4) + 4;
     }
   }
-  //print out extracted values
+   
   Serial.println(r_new);
   Serial.println(g_new);
   Serial.println(b_new);
   //set the led color
-  if(server.arg(0) == "1" && server.arg(1) == "1") {
-    Serial.print("Setting secondary");
-    setColorTo(0, r_new, g_new, b_new);
-  } else if(server.arg(0) == "0" && server.arg(1) == "1"){
-    Serial.println("Setting primary");
-    setColorTo(1, r_new, g_new, b_new);
-  } else {
-    setColorTo(1, r_new, g_new, b_new);
-    setColorTo(0, r_new, g_new, b_new);
-  }
+  CRGB color = CRGB( g_new, b_new, b_new );
+  fill_solid(leds, NUM_LEDS, color);
 }
 
 
@@ -110,105 +110,12 @@ void checkForBool() {
 }
 
 void randomColor(){
+  Serial.println("Random");
   effectOn = false;
   int red, green, blue;
-  red = random(1024);
-  green = random(1024);
-  blue = random(1024);
-  setColorTo(1, red, green, blue);
-  setColorTo(0, red, green, blue);
+  red = random(255);
+  green = random(255);
+  blue = random(255);
+  fill_solid(leds, NUM_LEDS, CRGB(red, green, blue));
   server.send(200);
-}
-
-
-void loopRGB() {
-  effectOn = false;
-  server.send(200);
-  effectOn = true;
-  setColorTo(0, 1024, 1024, 1024);
-  setColorTo(1, 1024, 1024, 1024);
-  while(effectOn){
-    flashColor(R, R2);
-    flashColor(G, G2);
-    flashColor(B, B2);
-  }
-}
-
-
-void rainbow() {
-  effectOn = false;
- Serial.print("comissing rainbows");
- float speed = 50;
- server.send(200);
- effectOn = true;
- while(effectOn){
-   setColorTo(1, 4, 1024, 1024);//RED
-   setColorTo(0, 72, 504, 75);//Violet
-   server.handleClient();
-   delay(speed);
-   setColorTo(1, 4, 364, 1024);//Orange
-   setColorTo(0, 4, 1024, 1024);//RED
-   server.handleClient();
-   delay(speed);
-   setColorTo(1, 4, 4, 1024);//Yellow
-   setColorTo(0, 4, 364, 1024);//Orange
-   server.handleClient();
-   delay(speed);
-   setColorTo(1, 1024, 4, 1024);//Green
-   setColorTo(0, 4, 4, 1024);//Yellow
-   server.handleClient();
-   delay(speed);
-   setColorTo(1, 1024, 1024, 4);//Blue
-   setColorTo(0, 1024, 4, 1024);//Green
-   server.handleClient();
-   delay(speed);
-   setColorTo(1, 724, 1024, 504);//Indigo
-   setColorTo(0, 1024, 1024, 4);//Blue
-   server.handleClient();
-   delay(speed);
-   setColorTo(1, 72, 504, 75);//Violet
-   setColorTo(0, 724, 1024, 504);//Indigo
-   server.handleClient();
-   delay(speed);
- }
-}
-
-void setColorTo(int led, int redPin, int greenPin, int bluePin){
-    if(led == 0){
-     analogWrite(R, redPin);
-     analogWrite(G, greenPin);
-     analogWrite(B, bluePin);
-    }else{
-     analogWrite(R2, redPin);
-     analogWrite(G2, greenPin);
-     analogWrite(B2, bluePin);
-    }
-    
-}
-
-void flashColor(int led, int led2){
-    analogWrite(led, 1024);
-    analogWrite(led2, 1024);
-    Serial.println("1024 -> 0");
-      for(int x=1024;x>2;x= x-2){
-        if(effectOn){
-          server.handleClient();
-          analogWrite(led, x);
-          analogWrite(led2, x);
-          delay(5);
-        }
-        if(x<=4){
-          Serial.println("0 -> 1024");
-          for(int i=0;i<1024; i= i+2){
-            if(effectOn){
-              server.handleClient();
-              analogWrite(led, i);
-              analogWrite(led2, i);
-              delay(5);
-            }
-          }
-        }
-      }
-    analogWrite(led, 1024);
-    analogWrite(led2, 1024);
 }
